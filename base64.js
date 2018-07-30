ed2f82aa555385cad583cfe991b2160aa93702f3
Mathew Fleisch <mathew.fleisch@gmail.com>
2018-07-29 22:02:59 -0700

    More structure for base64 puzzle

--- base64.js
+++ base64.js
@@ -1,4 +1,42 @@
 'use strict'
-const base64 = require('base-64')
+/**
+ * Valid URL: file=/www/new-agents/roster.txt
+ * &sig=BE3663BE6AF17EF57B2671E7C73069076151A5F72923E85CC5E34CD68A443E2D
+ * Desired file: /www/private/.htpasswd
+ * Response format: b64-encoded-query-string<space>b64-encoded-signature
+ * 30 Jul 2018 04:21:35 GMT « daemon -> player »
+ */
 
-console.log(base64.encode('this is a test'))
+const base64 = require('base-64')
+const sha256 = require('sha256')
+
+// Comparison string
+const compare = 'BE3663BE6AF17EF57B2671E7C73069076151A5F72923E85CC5E34CD68A443E2D'
+
+
+// Failed attempts
+var stringMatches = [
+  sha256(base64.encode('file=/www/new-agents/roster.txt')),
+  sha256(base64.encode('/www/new-agents/roster.txt')),
+  sha256('roster.txt'),
+  sha256(base64.encode('roster.txt'))
+]
+
+// Example hashes
+console.log(`base64: ${base64.encode('this is a test')}`)
+console.log(`sha256: ${sha256('this is another test')}`)
+
+// Compare strings
+for (var i = 0; i < stringMatches.length; i++) {
+  console.log(`Compare: ${compareString(compare, stringMatches[i].toUpperCase())}`)
+}
+
+
+function compareString(compare, thatString) {
+  console.log(`${thatString} ?== ${compare}`)
+  if (compare === thatString) {
+    return 'they are the same!'
+  } else {
+    return 'no match found'
+  }
+}
