b545915e9548aadc57a5e74cfba57c7d10c1ff92
Mathew Fleisch <mathew.fleisch@gmail.com>
2018-07-29 22:45:31 -0700

    Add some more examples

--- base64.js
+++ base64.js
@@ -13,15 +13,16 @@ const compare = 'BE3663BE6AF17EF57B2671E7C73069076151A5F72923E85CC5E34CD68A443E2
 
 const compareFile = '/www/new-agents/roster.txt'
 const compareQuery = `file=${compareFile}`
+const goal = '/www/private/.htpasswd'
 const hr = '-'.repeat(80)
 
 // Failed attempts
-var stringMatches = [
-  sha256(compareQuery),
-  sha256(base64.encode(compareQuery)),
-  sha256(compareFile),
-  sha256(base64.encode(compareFile))
-]
+var stringMatches = {
+  'sha256(compareQuery)': sha256(compareQuery).toUpperCase(),
+  'sha256(base64.encode(compareQuery))': sha256(base64.encode(compareQuery)).toUpperCase(),
+  'sha256(compareFile)': sha256(compareFile).toUpperCase(),
+  'sha256(base64.encode(compareFile))': sha256(base64.encode(compareFile)).toUpperCase()
+}
 
 // Example hashes
 console.log(`base64.encode('this is a test'): ${base64.encode('this is a test')}`)
@@ -29,13 +30,30 @@ console.log(`sha256('this is another test'): ${sha256('this is another test')}`)
 
 console.log(hr)
 
+// Setup
+console.log(`compareFile: ${compareFile}`)
+console.log(`compareQuery: ${compareQuery}`)
+
+console.log(hr)
+
+// Display possible matches
+console.log(JSON.stringify(stringMatches, null, 2))
+
+console.log(hr)
+
+// Show how the function works by itself
+console.log("Try comparing one string (don't forget to toUpperCase() the sha256 strings):")
+console.log(compareString(compare, sha256("This probably won't work").toUpperCase()))
+
+console.log(hr)
+
 // Compare strings
-for (var i = 0; i < stringMatches.length; i++) {
-  console.log(`Compare: ${compareString(compare, stringMatches[i].toUpperCase())}`)
+for (var thisComparison in stringMatches) {
+  console.log(`Compare [${thisComparison}]: ${compareString(compare, stringMatches[thisComparison].toUpperCase())}`)
   console.log(hr)
 }
 
-console.log(`Sanity Check: ${compareString(compare, compare.toUpperCase())}`)
+console.log(`Sanity Check: ${compareString(compare, compare)}`)
 
 function compareString(compare, thatString) {
   console.log('Comparing:')
