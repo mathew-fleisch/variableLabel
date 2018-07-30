765083af764a8751f2cae0afc2cda3e96ea81e30
Mathew Fleisch <mathew.fleisch@gmail.com>
2018-07-29 22:55:27 -0700

    Add comments to index.js

--- index.js
+++ index.js
@@ -8,20 +8,30 @@ const compare = "Agent 3oH ID "
 var o = [], n, padded
 var thisString = ''
 var track = []
+
+// Loop through first 10,000 numbers
 for (n = 0; n <= 9999; n++) {
+  // Pad numbers with zeros
   padded = ('000'+n).slice(-4)
+
+  // Build string to hash
   thisString = `${compare}${padded}`
+
+  // compare this string to comparison string
   if (compareString(goal, thisString)) {
+    // Put all answers in a new array
     track.push(thisString)
     console.log("^ THAT ONE!!! ^")
-    // break
   }
 }
+
+// Now that all of the answers are in the track variable, display them
 console.log('All the answers:')
 for (var i = 0; i < track.length; i++) {
   console.log(`${track[i]}: ${crc(track[i])}`)
 }
 
+// Compare a base string to a comparison string, by first hashing
 function compareString(goal, thatString) {
   var transformed = crc(thatString)
   for (var i = 0; i < goal.length; i++) {
