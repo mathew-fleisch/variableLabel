86786dfdf494e7b9d6996e2edec049e71a79e786
Mathew Fleisch <mathew.fleisch@gmail.com>
2018-07-29 22:23:07 -0700

    Clean up some variables

--- base64.js
+++ base64.js
@@ -3,8 +3,6 @@
  * Valid URL: file=/www/new-agents/roster.txt
  * &sig=BE3663BE6AF17EF57B2671E7C73069076151A5F72923E85CC5E34CD68A443E2D
  * Desired file: /www/private/.htpasswd
- * Response format: b64-encoded-query-string<space>b64-encoded-signature
- * 30 Jul 2018 04:21:35 GMT « daemon -> player »
  */
 
 const base64 = require('base-64')
@@ -13,26 +11,34 @@ const sha256 = require('sha256')
 // Comparison string
 const compare = 'BE3663BE6AF17EF57B2671E7C73069076151A5F72923E85CC5E34CD68A443E2D'
 
+const compareFile = '/www/new-agents/roster.txt'
+const compareQuery = `file=${compareFile}`
+const hr = '-'.repeat(100)
 
 // Failed attempts
 var stringMatches = [
-  sha256('file=/www/new-agents/roster.txt'),
-  sha256(base64.encode('file=/www/new-agents/roster.txt')),
-  sha256('/www/new-agents/roster.txt'),
-  sha256(base64.encode('/www/new-agents/roster.txt')),
-  sha256('roster.txt'),
-  sha256(base64.encode('roster.txt'))
+  sha256(compareQuery),
+  sha256(base64.encode(compareQuery)),
+  sha256(compareFile),
+  sha256(base64.encode(compareFile))
 ]
+
+console.log(hr)
 
 // Example hashes
 console.log(`base64.encode('this is a test'): ${base64.encode('this is a test')}`)
 console.log(`sha256('this is another test'): ${sha256('this is another test')}`)
 
+console.log(hr)
+
 // Compare strings
 for (var i = 0; i < stringMatches.length; i++) {
   console.log(`Compare: ${compareString(compare, stringMatches[i].toUpperCase())}`)
+  console.log(hr)
 }
+console.log(hr)
 
+console.log(`Sanity Check: ${compareString(compare, compare.toUpperCase())}`)
 
 function compareString(compare, thatString) {
   console.log(`${thatString} ?== ${compare}`)
